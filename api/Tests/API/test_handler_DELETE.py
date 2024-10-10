f7d79eef5d3e6c6df89919e7773b2725a0aaaf3f
safouuwa <safou1406@hotmail.com>
2024-10-10 23:57:58 +0200

    API DELETE, GET, en POST tests volledig werkend

--- api/Tests/API/test_handler_DELETE.py
+++ api/Tests/API/test_handler_DELETE.py
@@ -18,115 +18,120 @@ BASE_URL = "http://localhost:3000/api/v1"
 
 class TestApiHandlerDELETE(unittest.TestCase):
 
+    def setUp(self):
+        self.headers = {
+        "API_KEY": "a1b2c3d4e5"
+        }
+
     def test_delete_client(self):
         client_id = 1
-        response = httpx.delete(f"{BASE_URL}/clients/{client_id}")
-        self.assertEqual(response.status_code, 204) 
+        response = httpx.delete(f"{BASE_URL}/clients/{client_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200) 
         
-        response = httpx.get(f"{BASE_URL}/clients/{client_id}")
+        response = httpx.get(f"{BASE_URL}/clients/{client_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         client = json.loads(response.text)
-        self.assertEqual(client, "null")
+        self.assertEqual(client, None)
 
     def test_delete_inventory(self):
         inventory_id = 1
-        response = httpx.delete(f"{BASE_URL}/inventories/{inventory_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/inventories/{inventory_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/inventories/{inventory_id}")
+        response = httpx.get(f"{BASE_URL}/inventories/{inventory_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         inventory = json.loads(response.text)
-        self.assertEqual(inventory, "null")
+        self.assertEqual(inventory, None)
 
     def test_delete_item_group(self):
         item_group_id = 1
-        response = httpx.delete(f"{BASE_URL}/item_groups/{item_group_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/item_groups/{item_group_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/item_groups/{item_group_id}")
+        response = httpx.get(f"{BASE_URL}/item_groups/{item_group_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         item_group = json.loads(response.text)
-        self.assertEqual(item_group, "null")
+        self.assertEqual(item_group, None)
 
     def test_delete_item_line(self):
         item_line_id = 1
-        response = httpx.delete(f"{BASE_URL}/item_lines/{item_line_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/item_lines/{item_line_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/item_lines/{item_line_id}")
+        response = httpx.get(f"{BASE_URL}/item_lines/{item_line_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         item_line = json.loads(response.text)
-        self.assertEqual(item_line, "null")
+        self.assertEqual(item_line, None)
 
     def test_delete_item(self):
         item_id = "P000001"
-        response = httpx.delete(f"{BASE_URL}/items/{item_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/items/{item_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/items/{item_id}")
+        response = httpx.get(f"{BASE_URL}/items/{item_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         item = json.loads(response.text)
-        self.assertEqual(item, "null")
+        self.assertEqual(item, None)
 
     def test_delete_location(self):
         location_id = 1
-        response = httpx.delete(f"{BASE_URL}/locations/{location_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/locations/{location_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/locations/{location_id}")
+        response = httpx.get(f"{BASE_URL}/locations/{location_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         location = json.loads(response.text)
-        self.assertEqual(location, "null")
+        self.assertEqual(location, None)
 
     def test_delete_order(self):
         order_id = 1
-        response = httpx.delete(f"{BASE_URL}/orders/{order_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/orders/{order_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/orders/{order_id}")
+        response = httpx.get(f"{BASE_URL}/orders/{order_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         order = json.loads(response.text)
-        self.assertEqual(order, "null")
+        self.assertEqual(order, None)
 
     def test_delete_shipment(self):
         shipment_id = 1
-        response = httpx.delete(f"{BASE_URL}/shipments/{shipment_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/shipments/{shipment_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/shipments/{shipment_id}")
+        response = httpx.get(f"{BASE_URL}/shipments/{shipment_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         shipment = json.loads(response.text)
-        self.assertEqual(shipment, "null")
+        self.assertEqual(shipment, None)
 
     def test_delete_supplier(self):
         supplier_id = 1
-        response = httpx.delete(f"{BASE_URL}/suppliers/{supplier_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/suppliers/{supplier_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/suppliers/{supplier_id}")
+        response = httpx.get(f"{BASE_URL}/suppliers/{supplier_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         supplier = json.loads(response.text)
-        self.assertEqual(supplier, "null")
+        self.assertEqual(supplier, None)
 
     def test_delete_transfer(self):
         transfer_id = 1
-        response = httpx.delete(f"{BASE_URL}/transfers/{transfer_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/transfers/{transfer_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/transfers/{transfer_id}")
+        response = httpx.get(f"{BASE_URL}/transfers/{transfer_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         transfer = json.loads(response.text)
-        self.assertEqual(transfer, "null")
+        self.assertEqual(transfer, None)
 
     def test_delete_warehouse(self):
         warehouse_id = 1
-        response = httpx.delete(f"{BASE_URL}/warehouses/{warehouse_id}")
-        self.assertEqual(response.status_code, 204)
+        response = httpx.delete(f"{BASE_URL}/warehouses/{warehouse_id}", headers=self.headers)
+        self.assertEqual(response.status_code, 200)
         
-        response = httpx.get(f"{BASE_URL}/warehouses/{warehouse_id}")
+        response = httpx.get(f"{BASE_URL}/warehouses/{warehouse_id}", headers=self.headers)
         self.assertEqual(response.status_code, 200)
         warehouse = json.loads(response.text)
-        self.assertEqual(warehouse, "null")
+        self.assertEqual(warehouse, None)
 
 
 if __name__ == "__main__":
