53e805aab8765d93b56afd43a58fe06263a690a3
safouuwa <safou1406@hotmail.com>
2024-11-19 10:59:44 +0100

    Minimal Webapi aangemaakt
    ervoor gezorgd dat de api geen browser meer opent + Serving message gefixt

--- C#api/Program.cs
+++ C#api/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.PropertyNamingPolicy = null;  // Disable camelCase
     });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // Register your services
 builder.Services.AddSingleton<AuthProvider>();
@@ -24,19 +23,15 @@ var app = builder.Build();
 // Initialize your providers
 AuthProvider.Init();
 DataProvider.Init();
-// NotificationSystem.Start();
+NotificationSystem notisys = new NotificationSystem();
+NotificationSystem.Start();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Urls.Add("http://localhost:3000/");
 
+notisys.Push("Serving on PORT 3000");
 app.Run();
 Console.WriteLine("Hello");
