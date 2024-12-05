8f9cc2573e481860484a9b6a95788d29973f9355
safouuwa <safou1406@hotmail.com>
2024-12-05 16:16:55 +0100

    Requestlogging werkend
    voor elke request die gemaakt wordt met een geldige user, waarvan de model transfers, orders, shipments en items. De request die gemaakt is, door welke user, bij welke model de request behoord, de statuscode van de response en het datum+tijdstip dat het request gemaakt is.

--- C#api/Program.cs
+++ C#api/Program.cs
@@ -32,6 +32,19 @@ app.UseAuthorization();
 app.MapControllers();
 app.Urls.Add("http://localhost:3000/");
 
+app.Use(async (context, next) =>
+{
+    await next.Invoke();
+    var path = context.Request.Path.Value;
+    var pathParts = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+    if (pathParts.Length > 2 && new[] { "transfers", "shipments", "orders", "items" }.Contains(pathParts[2]))
+    {
+        var user = AuthProvider.GetUser(context.Request.Headers["API_KEY"]);
+        if (user == null) return;
+        await File.AppendAllTextAsync("C#api/RequestLogs/RequestLogs.txt", $"{context.Request.Path} made by {user.App}; Model: {pathParts[2]}; StatusCode: {context.Response.StatusCode}; Date and Time: {DateTime.UtcNow.AddHours(1)}\n");
+    }
+});
+
 notisys.Push("Serving on PORT 3000");
 app.Run();
-Console.WriteLine("Hello");
