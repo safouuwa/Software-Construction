4c124719d523c6365598185da3971bae35133552
safouuwa <safou1406@hotmail.com>
2024-12-06 11:51:16 +0100

    Nieuwere path met minder hard coding

--- C#api/Program.cs
+++ C#api/Program.cs
@@ -36,14 +36,15 @@ app.Use(async (context, next) =>
 {
     await next.Invoke();
     var path = context.Request.Path.Value;
-    var filepath = Path.Combine(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName).FullName).FullName, "C#api/RequestLogs/RequestLogs.txt").Replace('\\', '/');
+    string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    string logFilePath = Path.GetFullPath(Path.Combine(baseDirectory, "..", "..", "..", "C#api", "RequestLogs", "RequestLogs.txt"));    
     var pathParts = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
 
     if (pathParts.Length > 2 && new[] { "transfers", "shipments", "orders", "items" }.Contains(pathParts[2]))
     {
         var user = AuthProvider.GetUser(context.Request.Headers["API_KEY"]);
         if (user == null) return;
-        await File.AppendAllTextAsync(filepath, $"{context.Request.Method} {context.Request.Path} made by {user.App}; Model: {pathParts[2]}; StatusCode: {context.Response.StatusCode}; Date and Time: {DateTime.UtcNow.AddHours(1)}\n");
+        await File.AppendAllTextAsync(logFilePath, $"{context.Request.Method} {context.Request.Path} made by {user.App}; Model: {pathParts[2]}; StatusCode: {context.Response.StatusCode}; Date and Time: {DateTime.UtcNow.AddHours(1)}\n");
     }
 });
 
